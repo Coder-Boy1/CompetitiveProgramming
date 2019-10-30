f3038856607d52b8ba7893b8fc78539a01aabb00
Coder-Boy1 <43712886+Coder-Boy1@users.noreply.github.com>
2019-10-31 00:44:34 +0530

    Update 1006A. Adjacent Replacements.cpp

--- CodeForces/1006A. Adjacent Replacements.cpp
+++ CodeForces/1006A. Adjacent Replacements.cpp
@@ -1,6 +1,7 @@
 /*
   Idea:
     - If the number is odd print it as is, otherwise print it -1.
+  Because odd elements remains the same after the described algorithm but even elemenets are decreased by 1.
 */
 
 #include <bits/stdc++.h>
@@ -17,6 +18,18 @@ int main() {
   
   for(int i = 0; i < n; ++i)
     printf("%s%d", i == 0 ? "" : " ", a[i] - (a[i] % 2 == 0));
+  /*
+  This is same as above but easier to understand
+  for(int i=0;i<n;i++)
+  {
+    if(a[i]%2)
+      cout<<a[i];
+    else
+      cout<<a[i]-1;
+    cout<<" ";
+   }
+
+*/
   puts("");
   
   return 0;
